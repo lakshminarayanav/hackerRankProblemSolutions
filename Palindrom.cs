a3c309da480d0f3c0310df9ed05b2537949cb998
lakshminarayanav <lakshminarayana.vutla@hotmail.com>
2021-01-26 17:14:56 -0500

    HackerRank problem solutions
    C# Solutions for HackerRank Problems
    Problem #1 - get Countries by given minimum Population
    Problem #2 - print number of possible scattered palindrome sub strings for each string in given list of strings.
    problem #3 - print maximum of minimum value in sub sets of size x in given set of n elements, where 1 <= x <= n

---
+++ Palindrom.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HackerRankSolutions
+{
+    public class Palindrom
+    {
+
+        public static bool isScatteredPalindrom(string s)
+        {
+            var distinctChars = s.Distinct();
+            List<char> inputStr = s.ToCharArray().ToList();
+            int distinctCharOccurences = 0;
+            foreach (Char c in distinctChars)
+            {
+                if ((inputStr.FindAll(ch => ch == c).Count % 2 == 1))
+                {
+                    distinctCharOccurences++;
+                }
+            }
+
+
+            return distinctCharOccurences > 1 ? false : true;
+
+        }
+
+        public static int getScatteredPalindromCount(string s)
+        {
+            int count = 0;
+            string substr = string.Empty;
+            for (int i = 0; i < s.Length; i++)
+            {
+                for (int j = 1; i + j <= s.Length; j++)
+                {
+                    substr = s.Substring(i, j);
+                    if (isScatteredPalindrom(substr))
+                    {
+                        count++;
+                        Console.WriteLine(substr);
+                    }
+                }
+
+            }
+
+            Console.WriteLine($"total possible palindroms is : {count}");
+            return count;
+        }
+
+        public static List<int> ListOfScatteredPalindroms(List<string> input)
+        {
+            List<int> result = new List<int>();
+            foreach (string s in input)
+            {
+                result.Add(getScatteredPalindromCount(s));
+            }
+            return result;
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using HackerRankSolutions;
+
+namespace Samples
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+
+
+            #region Problem #1 - get Countries by given minimum Population
+            int minPopulation = 300;
+            int result = HRWebRequest.getCountries("un", minPopulation);
+            Console.WriteLine($"number of countries having population more than {minPopulation} is : {result}");
+            #endregion
+
+            
+            
+            
+            #region Problem #2 - print number of possible scattered palindrom sub strings for each string in given list of strings.
+
+            List<string> inputStrings = new List<string> { "bbrrg", "abc", "onino", "brrbrg" };
+            var listCount = Palindrom.ListOfScatteredPalindroms(inputStrings);
+            foreach (int c in listCount)
+            {
+                Console.WriteLine("Count is" + c);
+            }
+            #endregion
+
+            #region problem #3 - print maximum of minimum value in sub sets of size x in given set of n elements, where 1 <= x <= n 
+
+            int[] inputset = { 1, 2, 2, 3, 4, 6 };
+
+            int space = 1;
+            int maxValue = SubSets.findMaximumOfMinimumInSubSets(space, inputset);
+            Console.WriteLine($"Maximum of Minimum values of each subset is {maxValue}");
+
+            #endregion
+
+        }
+    }
+
+
+}
